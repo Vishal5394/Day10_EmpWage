a8fb5e7b3d44f13f6871ccde244019aee238e21f
vishal5394 <vishaljadyal@gmail.com>
2022-07-31 23:48:58 +0530

    Added: Calculation Til Max Hours Or Days Reached

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,33 +1,44 @@
 package com.BridgeLabz.EmpWage;
 
 public class  EmployeeWageComputation {
-	public static void main(String[] args) {
-		 
-		        final int PART_TIME = 1;
-		        final int FULL_TIME = 2;
-		        final int WAGE_PER_HR = 20;
-		        final int WORKING_DAYS = 20;
-		    
-		        int totalWage = 0;
-		        for (int day = 1; day <= WORKING_DAYS; day++)
-		        {
-		            int empType = (int) (Math.random() * 100) % 3;
-		            int workingHours =0;
-		            switch (empType)
-		            {
-		                case FULL_TIME:
-		                    workingHours = 8;
-		                    break;
-		                case PART_TIME:
-		                    workingHours = 4;
-		                    break;
-		                default:
-		            }
-		            int wage = workingHours * WAGE_PER_HR;
-		            System.out.println("Day " + day + " wage is:" + wage);
-		            totalWage += wage;
-		        }
-		        System.out.println("Total wage for a month is " + totalWage); 
+	public static void main(String[] args) { 
+		final int Is_Present = 1;
+		final int Is_PartTime = 2;
+		final int Emp_wage_per_hr = 20;
+		final int No_Of_Working_Days = 20;
+		final int No_Of_Working_hr = 100;
+
+
+			int  Employeehr = 0 , Daily_Wage = 0, Total_Wage = 0, TotalEmployeehr =0, TotalWorkingDays =0;
+			
+			while(TotalWorkingDays < No_Of_Working_Days && TotalEmployeehr <= No_Of_Working_hr) {
+			
+				int Attendance = (int)Math.floor(Math.random()*10)%3;
+			
+			switch (Attendance) {
+			 case Is_Present:
+				Employeehr = 8;
+					System.out.println("Employee is Present ");
+					break;
+			
+			case Is_PartTime :
+				Employeehr = 4;
+				 System.out.println("Employee is Present Part Time ");
+				 break;
+				 
+			default:
+				Employeehr = 0;
+			System.out.println("Employee is Absent ");
+			break;
+			}
+			
+			TotalEmployeehr += Employeehr;
+			Daily_Wage = Emp_wage_per_hr  *Employeehr;
+			Total_Wage = Daily_Wage * No_Of_Working_Days;
+			
+			System.out.println("Employee Daily Wage is = "+Daily_Wage);
+			System.out.println("Employee Monthly Total Wage is = "+Total_Wage);
 		 }
 		    
+	}
 	}
